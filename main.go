d54d8081c68c56036036483975054f3a9bb4f5b7
Mahmoud Mohamed <Me292949@gmail.com>
2024-08-30 18:33:21 +0000

    done fiber

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
         "fmt"
         "log"
+        "github.com/gofiber/fiber/v2"
 )
 
 func main() {
         app := fiber.New() 
+
+        app.Get("/", func(c *fiber.Ctx) error {
+                return c. Status (200). JSON (fiber.Map{"msg": "hello world"})
+                })
 
         fmt.Println("Hello worlgfghd")
         log.Fatal(app.Listen(":4000"))
